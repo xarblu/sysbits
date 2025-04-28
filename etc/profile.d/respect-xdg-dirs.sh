347bd7dbaf7d946bef7c7d3c52336d08092b81a0
Xarblu <xarblu@protonmail.com>
2025-04-28 21:06:23 +0200

    add more xdg dirs with xdg-ninja

--- etc/profile.d/respect-xdg-dirs.sh
+++ etc/profile.d/respect-xdg-dirs.sh
@@ -2,40 +2,49 @@
 # https://github.com/b3nj5m1n/xdg-ninja
 
 ### Define XDG dirs ###
-export XDG_DATA_HOME="${HOME}/.local/share"
-export XDG_CONFIG_HOME="${HOME}/.config"
-export XDG_STATE_HOME="${HOME}/.local/state"
 export XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_STATE_HOME="${HOME}/.local/state"
 
 ### Move things to XDG dirs ###
 # To XDG_DATA_HOME
-export LESSHISTFILE="${XDG_DATA_HOME}/less-history"
 export CARGO_HOME="${XDG_DATA_HOME}/cargo"
-export GNUPGHOME="${XDG_DATA_HOME}/gnupg"
+export DOTNET_CLI_HOME="${XDG_DATA_HOME}/dotnet"
 export DVDCSS_CACHE="${XDG_DATA_HOME}/dvdcss"
-export WINEPREFIX="${XDG_DATA_HOME}/wineprefixes/default"
+export GNUPGHOME="${XDG_DATA_HOME}/gnupg"
 export GOPATH="${XDG_DATA_HOME}/go"
+export LESSHISTFILE="${XDG_DATA_HOME}/less-history"
+export MYSQL_HISTFILE="${XDG_DATA_HOME}/mysql_history"
+export ROSWELL_HOME="${XDG_DATA_HOME}/roswell"
+export WINEPREFIX="${XDG_DATA_HOME}/wineprefixes/default"
 
 # To XDG_CONFIG_HOME
-export ZDOTDIR="${XDG_CONFIG_HOME}/zsh"
+#export _JAVA_OPTIONS="-Djava.util.prefs.userRoot=${XDG_CONFIG_HOME}/java"
+export ANDROID_HOME="${XDG_DATA_HOME}/android"
+export ANDROID_USER_HOME="${XDG_CONFIG_HOME}/android"
+export DOCKER_CONFIG="${XDG_CONFIG_HOME}/docker"
+export FFMPEG_DATADIR="${XDG_CONFIG_HOME}/ffmpeg"
 export GTK2_RC_FILES="${XDG_CONFIG_HOME}/gtk-2.0/gtkrc"
 export KDEHOME="${XDG_CONFIG_HOME}/kde"
 export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
-#export _JAVA_OPTIONS="-Djava.util.prefs.userRoot=${XDG_CONFIG_HOME}/java"
-export FFMPEG_DATADIR="${XDG_CONFIG_HOME}/ffmpeg"
-export ANDROID_USER_HOME="${XDG_CONFIG_HOME}/android"
-export ANDROID_HOME="${XDG_DATA_HOME}/android"
+export PARALLEL_HOME="${XDG_CONFIG_HOME}/parallel"
 export PYTHONSTARTUP="${XDG_CONFIG_HOME}/python/pythonrc" #requires pythonrc file but should only apply to interactive console anyway
 export RIPGREP_CONFIG_PATH="${XDG_CONFIG_HOME}/ripgreprc"
-export DOCKER_CONFIG="${XDG_CONFIG_HOME}/docker"
-export PARALLEL_HOME="${XDG_CONFIG_HOME}/parallel"
 export W3M_DIR="${XDG_DATA_HOME}/w3m"
+export ZDOTDIR="${XDG_CONFIG_HOME}/zsh"
 
 # To XDG_CACHE_HOME
 export ERRFILE="${XDG_CACHE_HOME}/X11/xsession-errors"
+export SQLITE_HISTORY="${XDG_CACHE_HOME}/sqlite_history"
+export TEXMFVAR="${XDG_CACHE_HOME}/texlive/texmf-var"
+
+# To XDG_STATE_HOME
+export NODE_REPL_HISTORY="${XDG_STATE_HOME}/node_repl_history"
 
 # To XDG_RUNTIME_DIR
 #export XAUTHORITY="$XDG_RUNTIME_DIR"/Xauthority #breaks xwayland
 
 ### Move things with aliases ###
-alias wget="wget --hsts-file=\${XDG_DATA_HOME}/wget-hsts"
+alias adb='HOME="$XDG_DATA_HOME"/android adb'
+alias wget='wget --hsts-file=${XDG_DATA_HOME}/wget-hsts'
