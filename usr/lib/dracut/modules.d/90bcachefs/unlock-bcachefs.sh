bdffad31feb6f9ad11a0f9e17bf29249e471e238
Xarblu <xarblu@protonmail.com>
2025-06-12 00:47:37 +0200

    move source to global location

--- usr/lib/dracut/modules.d/90bcachefs/unlock-bcachefs.sh
+++ usr/lib/dracut/modules.d/90bcachefs/unlock-bcachefs.sh
@@ -6,8 +6,9 @@ fi
 
 dev="${root#block:}"
 
+command -v ask_for_password > /dev/null || . /lib/dracut-crypt-lib.sh
+
 if bcachefs unlock -c "${dev}" >/dev/null 2>&1; then
-    command -v ask_for_password > /dev/null || . /lib/dracut-crypt-lib.sh
     ask_for_password \
         --cmd "bcachefs unlock ${dev}" \
         --prompt "Password (${dev})" \
