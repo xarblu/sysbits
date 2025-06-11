4550f38789211bfe930bf78602b2220afc39c4af
Xarblu <xarblu@protonmail.com>
2025-06-12 00:31:53 +0200

    no echo

--- usr/lib/dracut/modules.d/90bcachefs/unlock-bcachefs.sh
+++ usr/lib/dracut/modules.d/90bcachefs/unlock-bcachefs.sh
@@ -8,10 +8,10 @@ dev="${root#block:}"
 
 if bcachefs unlock -c "${dev}" >/dev/null 2>&1; then
     command -v ask_for_password > /dev/null || . /lib/dracut-crypt-lib.sh
-    info "Unlocking ${dev}:"
     ask_for_password \
         --cmd "bcachefs unlock ${dev}" \
-        --prompt "Password (${dev})"
+        --prompt "Password (${dev})" \
+        --tty-echo-off
 else
     info "${dev} not encrypted"
 fi
