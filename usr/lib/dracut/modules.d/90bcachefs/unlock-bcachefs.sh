084e4fe7f0ecf77b7799724f548194104d4ca6bc
Xarblu <xarblu@protonmail.com>
2025-06-12 00:03:51 +0200

    re-add limit to 5 tries

--- usr/lib/dracut/modules.d/90bcachefs/unlock-bcachefs.sh
+++ usr/lib/dracut/modules.d/90bcachefs/unlock-bcachefs.sh
@@ -8,9 +8,11 @@ dev="${root#block:}"
 
 if bcachefs unlock -c "${dev}" >/dev/null 2>&1; then
     info "Unlocking ${dev}:"
-    while true; do
-        bcachefs unlock "${dev}" && break
+    for _ in 1 2 3 4 5; do
+        bcachefs unlock "${dev}" && return 0
     done
+
+    die "Failed to unlock ${dev}"
 else
     info "${dev} not encrypted"
 fi
