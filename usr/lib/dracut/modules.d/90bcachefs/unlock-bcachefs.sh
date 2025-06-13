237865a088b856e714fa3f2ebe02464c0a06cbe2
Xarblu <xarblu@protonmail.com>
2025-06-13 08:42:30 +0200

    prettier unlock prompt

--- usr/lib/dracut/modules.d/90bcachefs/unlock-bcachefs.sh
+++ usr/lib/dracut/modules.d/90bcachefs/unlock-bcachefs.sh
@@ -7,7 +7,7 @@ fi
 dev="${root#block:}"
 
 if bcachefs unlock -c "${dev}" >/dev/null 2>&1; then
-    systemd-ask-password | bcachefs unlock "${dev}"
+    systemd-ask-password "Unlock ${dev}:" | bcachefs unlock "${dev}"
 else
     info "${dev} not encrypted"
 fi
