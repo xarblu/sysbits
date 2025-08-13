42a8e6535cb98b6d08fd9394bb2b98c22a4adc50
Xarblu <xarblu@protonmail.com>
2025-08-13 12:09:26 +0200

    refactor bcachefs dracut module

--- usr/lib/dracut/modules.d/90bcachefs/parse-bcachefs.sh
+++ usr/lib/dracut/modules.d/90bcachefs/parse-bcachefs.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-if [ "${fstype}" != bcachefs ]; then
-    return 0
-fi
+# shellcheck disable=SC1091 # file comes from dracut
+command -v getarg > /dev/null || . /lib/dracut-lib.sh
 
+# shellcheck disable=SC2154 # var comes from dracut
+case "${fstype}" in
+    bcachefs) ;;
+    auto) ;;
+    *) return 0 ;;
+esac
+
+# TODO: should this also be set for auto or just bcachefs?
+# shellcheck disable=SC2034 # var set for dracut
 rootok=1
--- usr/lib/dracut/modules.d/90bcachefs/unlock-bcachefs.sh
+++ usr/lib/dracut/modules.d/90bcachefs/unlock-bcachefs.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
-if [ "${fstype}" != bcachefs ]; then
-    return 0
-fi
+# shellcheck disable=SC1091 # file comes from dracut
+command -v getarg > /dev/null || . /lib/dracut-lib.sh
 
+# shellcheck disable=SC2154 # var comes from dracut
+case "${fstype}" in
+    bcachefs) ;;
+    auto)
+        warn "unlock-bcachefs: rootfstype unset or auto. Will try to unlock anyways."
+        ;;
+    *) return 0 ;;
+esac
+
+# shellcheck disable=SC2154 # var comes from dracut
 dev="${root#block:}"
 
-if bcachefs unlock -c "${dev}" >/dev/null 2>&1; then
+if bch_out="$(bcachefs unlock -c "${dev}" 2>&1)"; then
     systemd-ask-password "Unlock ${dev}:" | bcachefs unlock "${dev}"
 else
-    info "${dev} not encrypted"
+    info "unlock-bcachefs: ${bch_out}"
 fi
+
+unset dev bch_out
