9eb21fc835753cf05ef63dd710d8b28fb74db746
Xarblu <xarblu@protonmail.com>
2025-10-06 20:24:02 +0200

    add hack to keep rules printed

--- usr/share/sysbits/portage/ninja_prettifier.pl
+++ usr/share/sysbits/portage/ninja_prettifier.pl
@@ -4,14 +4,14 @@ use v5.35;
 
 # loosely based on cmake
 # https://github.com/Kitware/CMake/blob/f2afbff2c48a0479cc129c8eb2cc40e9181f0c51/Source/cmLocalUnixMakefileGenerator3.cxx#L1232
-sub prettify {
+sub prettify_description {
     my $rule = $_[0];
     my $key = $_[1];
     my $value = $_[2];
 
     # ANSI fmt e.g. 1;32 for bold green
     # clear by default
-    my $fmt = "0";
+    my $fmt = "";
 
     # C/CXX depend
     if ($rule =~ m/^(?:(?:C|CXX)_(?:SCAN|DYNDEP).*)$/) {
@@ -37,7 +37,24 @@ sub prettify {
         $fmt = "1;34";
     }
 
-    return "${key}\e[${fmt}m${value}\e[0m\n";
+    if ($fmt) {
+        return "${key}\e[${fmt}m${value}\e[0m\n";
+    } else {
+        return "${key}${value}\n";
+    }
+}
+
+# HACK:
+# Inject a space and backspace via printf before each command to keep the rule printed.
+# Ninja will print this on the line *after* a rule - essentially creating an empty line.
+# This empty the either get's replaced by the next rule or compiler output.
+# Only minor issue is that this will always generate a stray line at the last rule.
+sub keep_rule_hack {
+    my $rule = $_[0];
+    my $key = $_[1];
+    my $value = $_[2];
+
+    return $key . 'printf " \b" && ' . $value . "\n";
 }
 
 my $out = "";
@@ -57,7 +74,11 @@ while (my $line = <>) {
     # prettify rules
     if ($rule) {
         if ($line =~ m/^(\s+description\s*=\s*)(\S+(?:\s+\S+)*)$/) {
-            $line = prettify($rule, $1, $2);
+            $line = prettify_description($rule, $1, $2);
+        }
+
+        if ($line =~ m/^(\s+command\s*=\s*)(\S+(?:\s+\S+)*)$/) {
+            $line = keep_rule_hack($rule, $1, $2);
         }
     }
 
